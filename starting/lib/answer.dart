8e53f2d0a0ee2bce5e86c84dee1e1f5d61ac10d5
Appúchia <46938448+appuchias@users.noreply.github.com>
2020-05-23 18:57:03 +0200

    Dynamic answers

---
+++ starting/lib/answer.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final Function selectHandler;
+  final String answer;
+
+  Answer(this.selectHandler, this.answer);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      // height: 60,
+      // margin: EdgeInsets.only(top: 10, bottom: 10),
+      child: RaisedButton(
+        color: Colors.blue,
+        textColor: Colors.white,
+        child: Text(answer),
+        onPressed: selectHandler,
+      ),
+    );
+  }
+}
--- starting/lib/main.dart
+++ starting/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
+import 'question.dart';
+import 'answer.dart';
 
 // void main() => runApp(MyApp());
 
@@ -16,6 +17,30 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      "questionText": "Which is your favourite color?",
+      "answers": ["Blue", "Red", "Black", "White", "Yellow"]
+    },
+    {
+      "questionText": "Which is your favourite animal?",
+      "answers": ["Cat", "Dog", "Cheetah", "Tortoise"]
+    },
+    {
+      "questionText": "Which would you prefer?",
+      "answers": ["Know when you'll die", "Know how you'll die", "Neither"]
+    },
+    {
+      "questionText":
+          "If Wumpus invited you to a party but you prefer to hang out with Ned, what would you do?",
+      "answers": [
+        "Go with Wumpus because you value your friendship.",
+        "Go with Ned and turn your status to \"Invisible\" so Wumpus doesn't know.",
+        "Go hang out with Ned and tell Wumpus you feel a bit sick to party."
+      ]
+    },
+  ];
+
   var _questionIndex = 0;
 
   void _answerQuestion() {
@@ -23,41 +48,33 @@ class _MyAppState extends State<MyApp> {
       _questionIndex++;
     });
     print(_questionIndex);
+    if (_questionIndex < questions.length) {
+      print("We have more questions!");
+    }
   }
 
   @override
   Widget build(BuildContext ctx) {
-    var questions = [
-      "Which is your favourite color?",
-      "Which is your favourite animal?",
-      "Which would you prefer?",
-      "If Wumpus invited you to a party but you prefer to hang out with Ned, what would you do?",
-    ];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("My first app"),
-        ),
-        body: Column(
-          children: <Widget>[
-            Question(
-              questions[_questionIndex],
-            ),
-            RaisedButton(
-              child: Text("Option 1"),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text("Option 2"),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text("Option 3"),
-              onPressed: _answerQuestion,
-            ),
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: Text("My first app"),
+          ),
+          body: _questionIndex < questions.length
+              ? Column(
+                  children: <Widget>[
+                    Question(
+                      questions[_questionIndex]["questionText"],
+                    ),
+                    ...(questions[_questionIndex]["answers"] as List<String>)
+                        .map((answer) {
+                      return Answer(_answerQuestion, answer);
+                    }).toList()
+                  ],
+                )
+              : Center(
+                  child: Text("Finished!", textScaleFactor: 5,),
+                )),
     );
   }
 }
